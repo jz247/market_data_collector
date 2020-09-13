9b21a1b62ae31e767eacae2604ae806f1b8391cd
Jacek Bogocz <jacek.bogocz@gmail.com>
2020-09-13 13:52:46 +0200

    connect with dsn pool

--- db/db_pool.py
+++ db/db_pool.py
@@ -15,4 +15,16 @@ async def create_pool(*, dsn: str, min_conn: int = 2, max_conn: int = 10, **kwar
     return pool
 
 
-
+def get_pool(dsn: str, loop: asyncio.AbstractEventLoop) -> Pool:
+    attempts = 10
+    sleep_between_attempts = 3
+    for _ in range(attempts):
+        try:
+            pool = loop.run_until_complete(create_pool(dsn=dsn))
+        except Exception as e:
+            logger.exception(e)
+            time.sleep(sleep_between_attempts)
+        else:
+            return pool
+    raise Exception(f"Connected to the database using {dsn} "
+                    f"after {attempts * sleep_between_attempts} seconds")
